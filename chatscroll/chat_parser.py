ee558d646c39eb52a8956f7f8448b47fc65f9003
ramonca <ramonca@uio.no>
2025-07-09 17:10:15 +0200

    parse fields wip

--- chatscroll/chat_parser.py
+++ chatscroll/chat_parser.py
@@ -9,12 +9,47 @@ from typing import Any
 
 class ChatParser:
     def __init__(self, path_to_chat: pathlib.Path, app) -> None:
-        self.path_to_chat: pathlib.Path = path_to_chat
+        """
+
+        :param path_to_chat:
+        :param app:
+        """
+        self.path_to_chat: pathlib.Path = path_to_chat  # TODO: unzip if chat is uploaded zipped
         self.app: str = app
         self.chat: list[dict[str, Any]] = []
         self.raw_chat: str | Any = None
         self.participants: list[str] = []
-        self.metadata: dict[str, Any] = {}
 
-    def parse_chat(self):
-        print("Parsing chat...")
+    def parse_chat(self) -> None:
+        """
+        Parse chat file.
+        """
+        with open(self.path_to_chat, "r", encoding="utf-8") as f:
+            lines = f.readlines()
+            for line in lines:
+                try:
+                    # Separate timestamp from the rest; cast to datetime
+                    timestamp_str, rest = line.split(" - ", 1)
+                    timestamp = datetime.datetime.strptime(timestamp_str, '%d.%m.%Y, %H:%M')
+                    # Process participant & message fields
+                    participant, message = rest.split(":", 1)
+                    participant = participant.strip()
+                    message = message.strip()  # TODO: filter media & status change messages
+
+                    self.chat.append({
+                        "time": timestamp_str,
+                        "participant": participant,
+                        "message": message
+                    })
+
+                except ValueError:
+                    participant, message = None, None
+
+    # TODO: set participants
+    @property
+    def participants(self) -> list[str]:
+        return self.participants
+
+    # TODO: some method to print the json
+    def print_chat(self) -> None:
+        pass
